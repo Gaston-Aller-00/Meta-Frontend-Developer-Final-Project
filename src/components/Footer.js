f37202e8545411cc2c0c7036603800d8253be7e5
Gaston <gastonallerdev@gmail.com>
2024-01-09 09:19:52 -0300

    Footer  component

---
+++ src/components/Footer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import logo from "../images/Logo .svg";
+const Footer = () => {
+  return (
+    <footer>
+      <section>
+        <div className="company-info">
+          <img src={logo} alt="logo" />
+          <p>
+            We are family owned Mediterraneran restaurant focused, on
+            traditional recipes served with a modern twist.
+          </p>
+        </div>
+        <div>
+          <h3>Important Links!</h3>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Menu</a>
+            </li>
+            <li>
+              <a href="/">Reservation</a>
+            </li>
+            <li>
+              <a href="/">Order Online</a>
+            </li>
+            <li>
+              <a href="/">Login</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+            <h3>Contact</h3>
+            <ul>
+                <li>Address: <br/> 823 Towncity,USA</li>
+                <li>Phone: <br/> +11 232 324 912</li>
+                <li>Email: <br/>littlelemon@gmail.com</li>
+            </ul>
+        </div>
+        <div>
+            <h3>Social Media Links </h3>
+            <ul>
+                <li><a href="/" >Facebook</a></li>
+                <li><a href="/" >Intagram</a></li>
+                <li><a href="/" >Twitter</a></li>
+            </ul>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
